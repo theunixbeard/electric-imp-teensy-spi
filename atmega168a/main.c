bdf377e1631cbeac3d34ee8d0c8173079e9fa30b
Ben Gelsey <ben.gelsey@gmail.com>
2013-04-01 12:15:14 -0700

    Ported working code to atmega168a

--- atmega168a/main.c
+++ atmega168a/main.c
@@ -3,130 +3,251 @@
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 #include "string.h"
-#include <stdbool.h>
+#include "uart.h"
+#include "stdbool.h"
 
-void usart_init(unsigned short ubrr);
-void usart_out(char ch);
-char usart_in();
-void spi_slave_init(void);
+// TODO: Change to proper values!!!
+#define OUTLET1_CONFIG (DDRB |= (1<<6))
+#define OUTLET2_CONFIG (DDRB |= (1<<7))
+#define OUTLET3_CONFIG (DDRB |= (1<<5))
+#define OUTLET4_CONFIG (DDRC |= (1<<7))
+
+#define OUTLET1_ON  (PORTB |= (1<<6))
+#define OUTLET1_OFF (PORTB &= ~(1<<6))
+#define OUTLET2_ON  (PORTB |= (1<<7))
+#define OUTLET2_OFF (PORTB &= ~(1<<7))
+#define OUTLET3_ON  (PORTB |= (1<<5))
+#define OUTLET3_OFF (PORTB &= ~(1<<5))
+#define OUTLET4_ON  (PORTC |= (1<<7))
+#define OUTLET4_OFF (PORTC &= ~(1<<7))
+
+
+#define BAUD_RATE 2400
+
+#define OUTLET_COUNT 4
+#define MESSAGES_QUEUE_LENGTH 20
+
+#define MESSAGE1_PRODUCT_KEY_LENGTH 10
+#define MESSAGE1_OUTLET_NUMBER_LENGTH 4
+#define MESSAGE1_STATE_LENGTH 2
+#define MESSAGE1_OUTLET_ID_LENGTH 8
+#define MESSAGE1_LENGTH MESSAGE1_PRODUCT_KEY_LENGTH + MESSAGE1_OUTLET_NUMBER_LENGTH + MESSAGE1_STATE_LENGTH + MESSAGE1_OUTLET_ID_LENGTH + 2
+typedef struct message1 {
+  char board_product_key[MESSAGE1_PRODUCT_KEY_LENGTH];
+  char board_outlet_number[MESSAGE1_OUTLET_NUMBER_LENGTH];
+  char state[MESSAGE1_STATE_LENGTH];
+  char outlet_id[MESSAGE1_OUTLET_ID_LENGTH];
+} message1_t;
+
 void myprint(volatile char*);
-bool is_new_message(void);
-void simple_slave_receive(void);
+void process_message(void);
+void fill_in_message1(void);
+void print_message1(message1_t message);
+void outlets_init(void);
+void set_outlets_state(void);
+void send_message2(void);
+// 168a specific functions
+void print(char *);
+void pchar(char);
 
-#define FOSC 9830400 // Frequency of Oscillation AKA Clock Rate
-#define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
-
-#define SPI_BUFFER_LENGTH 500
-#define MESSAGE1_LENGTH 25
-
-volatile char spi_data[SPI_BUFFER_LENGTH];
-volatile int spi_data_index; // SPI next place to write data to
-volatile int processed_data_index;
-bool processing_message;
-int current_message_start_index;
+// Global Variables
+int messages_index;
+int replied_messages_index;
+message1_t messages[MESSAGES_QUEUE_LENGTH];
+bool outlet_states[OUTLET_COUNT];
+int atoi_length(const char*, int);
 
 int main(void) {
-  /* insert your hardware initialization here */
-  usart_init(MYUBRR);
-  spi_slave_init();
-  sei();
-  (DDRD |= (1<<1));
-  while(1){
-    (PORTD |= (1<<1));
-    (PORTD &= ~(1<<1));
-    
-    //simple_slave_receive();
-    /* If there is a message to process, do so */
-    /*
-    if (processing_message) {
-    
-    } else {
-      if (spi_data_index >) {
 
+  uart_init(BAUD_RATE);
+  outlets_init();
+  messages_index = 0;
+  replied_messages_index = 0;
+
+  uint8_t c;
+
+	while (1) {
+    /* Echo code for testing
+    if (uart_available()) {
+      c = uart_getchar();
+      print("Byte: ");
+      pchar(c);
+      pchar('\n');
+    }
+    */
+    if (uart_available()) {
+      process_message();
+    }
+    if (messages_index != replied_messages_index) {
+      print_message1(messages[replied_messages_index]);
+      // Check product key match, strncmp returns 0 if equal
+      if(!strncmp(messages[replied_messages_index].board_product_key, "1234567890", MESSAGE1_PRODUCT_KEY_LENGTH)) {
+        int outlet_number = atoi_length(messages[replied_messages_index].board_outlet_number, MESSAGE1_OUTLET_NUMBER_LENGTH);
+        if (outlet_number < OUTLET_COUNT) {
+          int outlet_state = atoi_length(messages[replied_messages_index].state, MESSAGE1_STATE_LENGTH);
+          if(outlet_state == 1 || outlet_state == 0) {
+            outlet_states[outlet_number] = outlet_state;
+            // Send message to imp
+            send_message2();
+          } else {
+            print("Bad outlet state\n");
+          }
+        } else {
+          print("Outlet number greater than number of outlets!\n");
+        }
+      } else {
+        print("Message for a different board\n");
       }
-    }*/
-  }
-  return 0;   /* never reached */
+      replied_messages_index++;
+      if(replied_messages_index >= MESSAGES_QUEUE_LENGTH) {
+        replied_messages_index = 0;
+      }
+    }
+    set_outlets_state();
+	}
 }
 
-void spi_slave_init(void) {
-  /* Set MISO output, all others input */
-  /* Should be 3 for teensy, 4 for atmega 168a*/
-  DDRB = (1<<4);
-  /* Enable SPI */
-  SPCR = (1<<SPE) | (1<<SPIE);
-  spi_data_index = 0;
-  processed_data_index = 0;
-  current_message_start_index = 0;
-  processing_message = false;
+int atoi_length(const char* str, int length) {
+  int ret = 0;
+  int i = 0;
+  for (i = 0; i < length; ++i) {
+    ret = 10*ret + *str - '0'; 
+    ++str;
+  }
+  return ret;
 }
 
-ISR(SPI_STC_vect) {
-//void simple_slave_receive(void) {
-  // while(!(SPSR & (1<<SPIF))); // For polling
-  spi_data[spi_data_index++] = SPDR;
-  SPDR = 'y';
-/*
-  if(spi_data[spi_data_index-1]  == '\0') {
-    myprint(spi_data);
-    //_delay_ms(5000);
+void outlets_init(void) {
+  OUTLET1_CONFIG;
+  OUTLET2_CONFIG;
+  OUTLET3_CONFIG;
+  OUTLET4_CONFIG;
+  int i = 0;
+  /* Replace with code to get actual states from DB !!! */
+  for(i = 0; i < OUTLET_COUNT; ++i) {
+    outlet_states[i] = true;
   }
-  if (spi_data_index == SPI_BUFFER_LENGTH) {
-    spi_data_index = 0;
+  set_outlets_state();
+}
+
+void set_outlets_state(void) {
+  if (outlet_states[0]) {
+    OUTLET1_ON;
+  } else {
+    OUTLET1_OFF;
   }
-*/
+  if (outlet_states[1]) {
+    OUTLET2_ON;
+  } else {
+    OUTLET2_OFF;
+  }
+  if (outlet_states[2]) {
+    OUTLET3_ON;
+  } else {
+    OUTLET3_OFF;
+  }
+  if (outlet_states[3]) {
+    OUTLET4_ON;
+  } else {
+    OUTLET4_OFF;
+  }
+
 }
 
 void myprint(volatile char *str) {
- /*
   while(*str != '\0') {
-    usart_out(*str);
-    str++;
-  }
-  */
-  int i = 0;
-  for (i = 0; i < 25; ++i) {
-    usart_out(*(str+i));
+    pchar(*str++);
   }
 }
 
-bool is_new_message(void) {
-  /* Determine if normal case or wrap around case*/
-  if (spi_data_index >= processed_data_index) {
-      /* In normal case, if more than 25 forward we're good*/
-    if (spi_data_index - MESSAGE1_LENGTH >= processed_data_index) {
-      return true;
-    } else {
-      return false;
+void process_message(void) {
+  uint8_t message_type;
+  if (uart_available() < MESSAGE1_LENGTH ) {
+      _delay_ms(1000); // So we have a full message to process
+  }
+  if (uart_available() < MESSAGE1_LENGTH ) {
+    print("Something went wrong, only have partial message!\n");
+  }
+  // First character should indicate message type
+  message_type = uart_getchar();
+  if (message_type != '!') {
+    print("Received unrecognized Message type!\n");
+  }
+  if (message_type == '!') {
+    fill_in_message1();
+  }
+  return;
+}
+
+void fill_in_message1(void) {
+  int i = 0;
+  uint8_t c;
+  for(i = 0; i < MESSAGE1_PRODUCT_KEY_LENGTH; ++i) {
+    messages[messages_index].board_product_key[i] = uart_getchar();
+  }
+  for(i = 0; i < MESSAGE1_OUTLET_NUMBER_LENGTH; ++i) {
+    messages[messages_index].board_outlet_number[i] = uart_getchar();
+  }
+  for(i = 0; i < MESSAGE1_STATE_LENGTH; ++i) {
+    messages[messages_index].state[i] = uart_getchar();
+  }
+  for(i = 0; i < MESSAGE1_OUTLET_ID_LENGTH; ++i) {
+    messages[messages_index].outlet_id[i] = uart_getchar();
+  }
+  c = uart_getchar();
+  if (c != '\0') {
+    print("Characte ending message was NOT a null... Possible Error! (message_index NOT incremented)\n");
+  } else {
+    messages_index++;
+    if(messages_index >= MESSAGES_QUEUE_LENGTH) {
+      messages_index = 0;
     }
   }
-    /* In wrap around case */
-    return true;
+  return;
 }
 
-/*
-usart_init - Initialize the USART port
-*/
-void usart_init(unsigned short ubrr) {
-  UBRR0 = ubrr ; // Set baud rate
-  UCSR0B |= (1 << TXEN0); // Turn on transmitter
-  UCSR0B |= (1 << RXEN0); // Turn on receiver
-  UCSR0C = (3 << UCSZ00); // Set for async . operation , no parity ,
-  // one stop bit , 8 data bits
-}
-/*
-usart_out - Output a byte to the USART0 port
-*/
-void usart_out(char ch) {
-  while ((UCSR0A & (1 << UDRE0)) == 0);
-  UDR0 = ch ;
-}
-/*
-usart_in - Read a byte from the USART0 and return it
-*/
-char usart_in() {
-  while (!(UCSR0A & (1 << RXC0)));
-  return UDR0 ;
+void print_message1(message1_t message) {
+  int i = 0;
+  print("Message1 Received: ");
+  print("\n\tProduct Key: ");
+  for(i = 0; i < MESSAGE1_PRODUCT_KEY_LENGTH; ++i) {
+    pchar(message.board_product_key[i]);
+  }
+  print("\n\tOutlet Number: ");
+  for(i = 0; i < MESSAGE1_OUTLET_NUMBER_LENGTH; ++i) {
+    pchar(message.board_outlet_number[i]);
+  }
+  print("\n\tState: ");
+  for(i = 0; i < MESSAGE1_STATE_LENGTH; ++i) {
+    pchar(message.state[i]);
+  }
+  print("\n\tOutlet Id: ");
+  for(i = 0; i < MESSAGE1_OUTLET_ID_LENGTH; ++i) {
+    pchar(message.outlet_id[i]);
+  }
+  print("\n\n");
 }
 
+void send_message2(void) {
+  
+  int i = 0;
+  uart_putchar('@');
+  for(i = 0; i < MESSAGE1_OUTLET_ID_LENGTH; ++i) {
+    uart_putchar(messages[replied_messages_index].outlet_id[i]);
+  }
+  for(i = 0; i < MESSAGE1_STATE_LENGTH; ++i) {
+    uart_putchar(messages[replied_messages_index].state[i]);
+  }
+  uart_putchar('\0');
+  return;
+}
+
+void pchar(char c) {
+  // Do nothing for now...
+  return;
+}
+
+void print(char * str) {
+  // Do nothing for now...
+  return;
+}
